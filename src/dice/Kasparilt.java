82f4ab0507376e2047cb3fabbc993bd9a9629dec
Karl Kadalipp <karl.kadalipp@gmail.com>
2016-06-21 18:37:43 +0300

    Changed a few names.

--- src/dice/Kasparilt.java
+++ src/dice/Kasparilt.java
@@ -12,19 +12,19 @@ public class Kasparilt {
 		// 4: 9.7%
 		// 5: 1.3%
 		int sides = 6; // täringul on 6 tahku
-		int dicesOriginal = 5; // nii mitme täringu kaupa viskan (nt 5 korraga)
+		int dicesOriginal = 5; // mitu täringut (nii mitme täringu kaupa viskan (nt 5 korraga))
 		int dicesTemp;
-		int triesOriginal = 3; // mitu korda võimalus täringuid visata
+		int triesOriginal = 3; // mitu proovi (e mitu korda võimalus täringuid visata)
 		int triesTemp;
-		int rolls = 100000; // viskan täringuid miljon korda kokku (per 1 täring)
+		int rolls = 1000000; // viskan täringuid miljon korda kokku (per 1 täring)
 		int series = rolls / (dicesOriginal * triesOriginal); // viskan täringut nii mitu seeriat (nt 5 täringut korraga)
 		
 		int rollResult = 0; // ühe veeretuse tulemus
 		int numbers[] = new int[5]; // massiiv täringute tulemuse jaoks
-		int howManyWantedNumbers = 0; // mitu täringut olen õigesti juba ära visanud
+		int alreadyCorrect = 0; // mitu täringut olen õigesti juba ära visanud
 		int totalTimesSuccessful = 0; // mitu korda olin edukas oma üldeesmärgi saavutamisel
 		
-		int howManyOfAll = 1; // mitu õiget peab olema kõigist (kas jahin 5t õiget 5st vmt).
+		int goalAmount = 5; // mitu õiget peab olema kõigist (kas jahin 5t õiget 5st vmt).
 		
 		// 5t täringut viskan 200 tuhat korda
 		for(int i = 0; i < series; i++){
@@ -32,7 +32,7 @@ public class Kasparilt {
 			triesTemp = triesOriginal; 
 			dicesTemp = dicesOriginal;
 			// senikaua viskan kuni visete korrad on otsas või pole ühtegi täringut visata
-			while(triesTemp > 0 && howManyWantedNumbers < howManyOfAll){ // NB! &&, muidu timestothrow läheb negatiivseks kuna pole veel saanud vajaminevaid tulemusi
+			while(triesTemp > 0 && alreadyCorrect < goalAmount){ // NB! &&, muidu timestothrow läheb negatiivseks kuna pole veel saanud vajaminevaid tulemusi
 				// viskan nii palju täringuid ükshaaval kui on täringuid ette nähtud (nt 5 asemel 3)
 				numbers = new int[dicesTemp];
 				for(int j=0;j<dicesTemp;j++){
@@ -41,24 +41,24 @@ public class Kasparilt {
 				} // for
 				for(int wantThisNumber : numbers){
 					if(wantThisNumber==4){
-						howManyWantedNumbers++;
+						alreadyCorrect++;
 						dicesTemp--;	
 					} // if
 				} // for
 				triesTemp--;
 				//System.out.println("timesToThrow " + tries + " result " + result);
-				if(howManyWantedNumbers == howManyOfAll){
+				if(alreadyCorrect == goalAmount){
 					totalTimesSuccessful++;
 				} // if
 			} // END while
 
 			//System.out.print("mitu korda saan veel visata: " + tries );
 			//System.out.println(" tulemused: " + Arrays.toString(numbers) );
-			howManyWantedNumbers=0;
+			alreadyCorrect=0;
 		} // for
 		
 		System.out.println("Kokku täringuid visata: " + rolls + " ja see tähendab, et on seeriaid: " + series);
-		System.out.println("Tahan " + dicesOriginal + "-st saada " + triesOriginal + "-e viskega " + howManyOfAll + ", tulemus: " + totalTimesSuccessful);
+		System.out.println(triesOriginal + " viset " + dicesOriginal + " täringuga ja vaja saada "+ goalAmount +" õiget, tulemus: " + totalTimesSuccessful);
 		System.out.println("Percentage: " +  ((double) totalTimesSuccessful / (double) series * 100) + "%");
 
 	}
